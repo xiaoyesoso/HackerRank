ad13fdf988ea68edaaf9603e919420a3ac44cc86
xiaoyesoso <zhuoshoujie@126.com>
2016-12-09 12:15:00 +0800

    add day2

---
+++ 30DaysofCode/scala/src/Day2/Solution.scala
@@ -0,0 +1,10 @@
+package Day2
+
+object Solution {
+  def main(args:Array[String]){
+    val meal = scala.io.StdIn.readLine().toDouble
+    val tip = scala.io.StdIn.readLine().toDouble
+    val rax = scala.io.StdIn.readLine().toDouble
+    print("The total meal cost is "+math.round(meal*(100+tip+rax)/100.0)+" dollars.")
+  }
+}
