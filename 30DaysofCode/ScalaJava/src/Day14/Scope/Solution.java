7a622022f16d2808e73047743f4c458bc9da9425
xiaoyesoso <zhuoshoujie@126.com>
2016-12-21 13:43:05 +0800

    add day 14

---
+++ 30DaysofCode/ScalaJava/src/Day14/Scope/Solution.java
@@ -0,0 +1,45 @@
+package Day14.Scope;
+
+import java.util.Scanner;
+
+import scala.collection.generic.BitOperations.Int;
+
+class Difference {
+	private int[] elements;
+	public int maximumDifference;
+
+	public Difference(int[] a) {
+		elements = a;
+	}
+
+	public void computeDifference() {
+		int Max = elements[0], Min = elements[0];
+		for (int i = 1; i < elements.length; ++i) {
+			if (Max < elements[i]) {
+				Max = elements[i];
+			} else if (elements[i] < Min) {
+				Min = elements[i];
+			}
+		}
+		maximumDifference = Max - Min;
+	}
+}
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		sc.close();
+
+		Difference difference = new Difference(a);
+
+		difference.computeDifference();
+
+		System.out.print(difference.maximumDifference);
+	}
+}
